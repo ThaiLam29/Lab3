9955ca5c2831622ce689c2ca62d1f6be43482a75
Thailam29 <lamntph34492@fpt.edu.vn>
2023-05-18 23:26:28 +0700

    ThaiLam

---
+++ app/src/main/java/com/example/lab3_ph34455/bai1.java
@@ -0,0 +1,14 @@
+package com.example.lab3_ph34455;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
---
+++ app/src/main/java/com/example/lab3_ph34455/bai3.java
@@ -0,0 +1,14 @@
+package com.example.lab3_ph34455;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bai3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contrainlayout);
+    }
+}
